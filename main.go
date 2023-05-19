7460b88830c8d3623b00c09fb0b8b6851590141d
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-05-19 19:51:49 -0300

    logs init

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/brnocorreia/go-movies-crud/src/configuration/logger"
 	"github.com/brnocorreia/go-movies-crud/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("About to start user application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,18 +1,26 @@
 package controller
 
 import (
+	"github.com/brnocorreia/go-movies-crud/src/configuration/logger"
 	"github.com/brnocorreia/go-movies-crud/src/configuration/validation"
 	"github.com/brnocorreia/go-movies-crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var UserRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&UserRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
+
 }
