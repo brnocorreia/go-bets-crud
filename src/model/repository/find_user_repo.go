a9eb12c39e9c038ac3817c6c03d30fcb78b7a942
Bruno Correia <bruninhocorreia2013@gmail.com>
2023-06-09 15:14:37 -0300

    Find user repo done

--- src/model/repository/find_user_repo.go
+++ src/model/repository/find_user_repo.go
@@ -53,3 +53,42 @@ func (ur *userRepository) FindUserByEmail(
 	)
 	return converter.ConvertEntityToDomain(*userEntity), nil
 }
+
+func (ur *userRepository) FindUserByID(
+	id string,
+) (model.UserDomainInterface, *rest_err.RestErr) {
+	logger.Info("Init findUserByID repository",
+		zap.String("journey", "findUserByID"))
+
+	collection_name := os.Getenv(MONGODB_USER_COLLECTION)
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	userEntity := &entity.UserEntity{}
+
+	filter := bson.D{{"_id", id}}
+	err := collection.FindOne(
+		context.Background(),
+		filter,
+	).Decode(userEntity)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			errorMessage := fmt.Sprintf("User not found with this id: %s", id)
+			logger.Error(errorMessage,
+				err,
+				zap.String("journey", "findUserByID"))
+			return nil, rest_err.NewNotFoundError(errorMessage)
+		}
+		errorMessage := "Error trying to find user by id."
+		logger.Error(errorMessage,
+			err,
+			zap.String("journey", "findUserByID"))
+		return nil, rest_err.NewInternalServerError(errorMessage, []rest_err.Causes{})
+	}
+
+	logger.Info("FindUserByID repository executed successfully",
+		zap.String("journey", "findUserByID"),
+		zap.String("userId", userEntity.ID.Hex()),
+	)
+	return converter.ConvertEntityToDomain(*userEntity), nil
+}
